c9821334c83b69d1e42a373bbefa457a1a67dea9
Mahfoudh AROUS <mahfoudh.arous@gmail.com>
2019-11-10 10:07:31 +0100

    update

--- sw.js
+++ sw.js
@@ -1,14 +1,19 @@
+// On install - caching the application shell
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('sw-cache').then(function(cache) {
+      // cache any static files that make up the application shell
       return cache.add('index.html');
     })
   );
 });
- 
+
+// On network request
 self.addEventListener('fetch', function(event) {
   event.respondWith(
+    // Try the cache
     caches.match(event.request).then(function(response) {
+      //If response found return it, else fetch again
       return response || fetch(event.request);
     })
   );
